03c29335bc74da98e15e8a6b6e25d5fcf17fc75b
chanrain <nerdtzek14@gmail.com>
2024-01-23 17:12:42 -0800

    cleaning up

--- src/tippyConfig.js
+++ src/tippyConfig.js
@@ -1,5 +1,6 @@
 // Create a global object to store the scam options
 var scamOptions = {};
+var modifierOptions = {};
 
 tippy('.grid-item:not(.road):not(.water):not(.natural):not(.empty):not(.beach):not(.residential):not(.industrial)', {
   trigger: 'click',
@@ -12,10 +13,17 @@ tippy('.grid-item:not(.road):not(.water):not(.natural):not(.empty):not(.beach):n
     setTimeout(function(){
       let buildingId = instance.reference.dataset.name;
       let selectBox = document.getElementById(buildingId+"_scam_options");
+      let modifierBox = document.getElementById(buildingId+"_modifier_options");
       if (selectBox) {
         // Store the value in the global object
         scamOptions[buildingId] = selectBox.value;
       }
+
+      if (modifierBox) {
+        // Store the value in the global object
+        modifierOptions[buildingId] = modifierBox.value;
+      }
+
       let buildingOptions = document.getElementById(buildingId+"_player_options");
       if (player.buildingsIndex.includes(buildingId)) {
         //buildingOptions.innerHTML = ownedBuildingOptions;
@@ -32,9 +40,14 @@ tippy('.grid-item:not(.road):not(.water):not(.natural):not(.empty):not(.beach):n
   onHide(instance) {
     let buildingId = instance.reference.dataset.name;
     let selectBox = document.getElementById(buildingId + "_scam_options");
+    let modifierBox = document.getElementById(buildingId + "_modifier_options");
     if (selectBox) {
       // Update the value in the global object
       scamOptions[buildingId] = selectBox.value;
+    }
+    if (modifierBox) {
+      // Update the value in the global object
+      modifierOptions[buildingId] = modifierBox.value;
     }
   }
 });
