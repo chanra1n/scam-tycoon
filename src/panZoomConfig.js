a1a3f275a95778370e7b7ffc6d0ff54a26afc66c
chanrain <nerdtzek14@gmail.com>
2024-01-25 12:43:16 -0800

    fixed a scrolling issue

--- src/panZoomConfig.js
+++ src/panZoomConfig.js
@@ -1,7 +1,53 @@
-var panZoomController = panzoom(document.getElementById('world-map'), {
+var worldMap = document.getElementById('world-map');
+var isHovering = false;
+
+worldMap.addEventListener('mouseover', function() {
+  isHovering = true;
+});
+
+worldMap.addEventListener('mouseout', function() {
+  isHovering = false;
+});
+
+var panZoomController = panzoom(worldMap, {
   bounds: true,
   maxZoom: 4,
   minZoom: 1,
   boundsPadding: 1,
+  beforeWheel: function(e) {
+    return !isHovering;
+  },
+  beforeMouseDown: function(e) {
+    return !isHovering;
+  },
 });
 
+panZoomController.on('panstart', function(e) {
+  if (!isHovering) return;
+
+});
+
+panZoomController.on('pan', function(e) {
+  if (!isHovering) return;
+
+});
+
+panZoomController.on('panend', function(e) {
+  if (!isHovering) return;
+
+});
+
+panZoomController.on('zoom', function(e) {
+  if (!isHovering) return;
+
+});
+
+panZoomController.on('zoomend', function(e) {
+  if (!isHovering) return;
+
+});
+
+panZoomController.on('transform', function(e) {
+  if (!isHovering) return;
+
+});
